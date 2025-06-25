0d091e620bcaa0bcc03f1ab452815213eb981536
BisonoPriya <bis.priyambodo@gmail.com>
2025-06-26 01:46:37 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,6 +26,10 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 $routes->group('produk-kategori', function($routes) {
     $routes->get('/', 'ProdukCategory::index');
     $routes->post('store', 'ProdukCategory::store');
